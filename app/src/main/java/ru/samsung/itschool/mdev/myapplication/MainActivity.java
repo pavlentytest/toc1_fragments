e03d9ee9d56fb0d6d3a11e199ae2e768042d84e1
Pavel <impetus@yandex.ru>
2021-12-24 17:40:02 +0300

    Initial commit

---
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package ru.samsung.itschool.mdev.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ru.samsung.itschool.mdev.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // ViewBinding
+    // https://developer.android.com/topic/libraries/view-binding
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new Fragment1());
+            }
+        });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new Fragment2());
+            }
+        });
+    }
+
+    public void loadFragment(Fragment f) {
+        FragmentManager fm = getSupportFragmentManager();
+
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frBody,f);
+        ft.addToBackStack(null); // кладем fragmentы в стек (для хистори)
+        ft.commit();
+
+    }
+}
